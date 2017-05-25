0d603d3fef6288168a7dd6eb5d73dc168eb46de3
hyeongchanshin <shch8909@gmail.com>
2017-05-25 22:24:52 +0900

    commit in class

--- class7sample/renderCode.R
+++ class7sample/renderCode.R
@@ -1,5 +1,14 @@
 if(!require(rmarkdown)) install.packages("rmarkdown")
 library(rmarkdown,lib.loc = "C:/Users/mrchypark/Documents/R/win-library/3.4")
 
-render(input = "C:/Users/mrchypark/Documents/project/dabrp_classnote/class7sample/pdftesthangul.Rmd",
+install.packages("devtools")
+library(devtools)
+devtools::install_github("bnosac/taskscheduleR")
+install.packages('miniUI')
+install.packages('shiny')
+
+library(miniUI)
+library(shiny)
+
+render(input = "C:/Users/User/Desktop/N Drive/RSQL_170406~170601/dabrp_classnote_170525_prac/class7sample/pdftesthangul.Rmd",
        encoding="UTF-8")
